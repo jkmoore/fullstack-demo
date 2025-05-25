e2c1f9cf7a2a39d5882778436261bbf7a180f759
jkmoore <jkm9@pdx.edu>
2025-05-25 14:54:50 +0900

    feat: basic setup and load hello world

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js",".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ],
+  devServer: {
+    static: "./dist",
+    hot: true,
+    open: true
+  },
+  mode: "development",
+};
